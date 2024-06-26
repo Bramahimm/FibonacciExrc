2d678afce976df29e7559d07bee35c2f58db624d
Bramahimm <149980338+Bramahimm@users.noreply.github.com>
2024-06-26 14:24:01 +0700

    Create FibExercise.cpp

---
+++ FibExercise.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+int rekursif(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return rekursif(n - 1) + rekursif(n - 2);
+}
+
+int dinamic(int n) {
+    vector<int> angka(n + 1);
+    angka[0] = 0;
+    ankga[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        angka[i] = angka[i - 1] + angka[i - 2];
+    }
+    return angka[n];
+}
+
+int main() {
+    int nilai[] = {10, 25, 30, 40, 50};
+    
+    for (int n : nilai) {
+        auto start = chrono::high_resolution_clock::now();
+        cout << "Fibonacci ke-" << n << " dengan rekursif: " << rekursif(n) << endl;
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double> elapsed = end - start;
+        cout << "Waktu eksekusi (rekursif): " << elapsed.count() << " detik" << endl;
+
+        start = chrono::high_resolution_clock::now();
+        cout << "Fibonacci ke-" << n << " dengan dynamic programming: " << dinamic(n) << endl;
+        end = chrono::high_resolution_clock::now();
+        elapsed = end - start;
+        cout << "Waktu eksekusi (dynamic programming): " << elapsed.count() << " detik" << endl;
+        
+        cout << "---------------------------------------------" << endl;
+    }
+    
+    return 0;
+}
